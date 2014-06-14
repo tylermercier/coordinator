1a4ed1dd6ee2e5677a143600a81dfdac8a7f649a
Tom Burns <tom.burns@jadedpixel.com>
2014-06-14 02:28:38 +0000

    use non-AR finder name as we need to convert to a Queue object first

--- lib/coordinator/base.rb
+++ lib/coordinator/base.rb
@@ -5,15 +5,15 @@ module Coordinator
     end
 
     def add_task(skill, task)
-      @queues.find_by_skill(skill).add_task(task)
+      @queues.queue_for_skill(skill).add_task(task)
     end
 
     def add_priority_task(skill, task)
-      @queues.find_by_skill(skill).add_priority_task(task)
+      @queues.queue_for_skill(skill).add_priority_task(task)
     end
 
     def remove_task(skill, task)
-      @queues.find_by_skill(skill).remove_task(task)
+      @queues.queue_for_skill(skill).remove_task(task)
     end
 
     def next_task(skills)
@@ -25,11 +25,11 @@ module Coordinator
     end
 
     def set_capacity(skill, capacity)
-      @queues.find_by_skill(skill).set_capacity(capacity)
+      @queues.queue_for_skill(skill).set_capacity(capacity)
     end
 
     def info(skill)
-      queue = @queues.find_by_skill(skill)
+      queue = @queues.queue_for_skill(skill)
       {
         "name" => queue.name,
         "capacity" => queue.capacity,
